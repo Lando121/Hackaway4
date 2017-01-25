84b093e41fbb115a41012959cb4c25969040b965
Emilio Lando <emilio.lando@gmail.com>
2017-01-25 17:26:52 +0100

    Javascript med APICall mot resRobot

---
+++ js/TimeTableAPI.js
@@ -0,0 +1,21 @@
+function APICall(name, callback) {
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    var url = "https://api.resrobot.se/v2/location.name?key=c894e228-9574-4474-b2dc-b09107b498e7&input=" + name;
+    var url2 = "https://api.resrobot.se/v2/location.name.json?key=<c894e228-9574-4474-b2dc-b09107b498e7>&input=centralen"
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.send(null);
+                  
+}
+
+function printResponse(response) {
+    console.log(response);
+}
+
+function getStation(name){
+    APICall(name, printResponse);
+}
