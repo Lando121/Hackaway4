6d6fe087a29e7f9e202297b838dbf1d3bb852fbc
Emilio Lando <emilio.lando@gmail.com>
2017-01-29 03:47:23 +0100

    mini

---
+++ firebasebranch/public/scripts/getStations.js
@@ -0,0 +1,45 @@
+var API_KEY = "dcef1be43f25481d969b1821aac7f0c5";
+var getStations = {};
+var listOfStations = [];
+
+function getData(callback){
+    callbackFunc = callback
+    for (i = 0; i<stations.length; i++){
+        platsuppslag(stations[i]);
+    }
+
+}
+
+getStations.generate = function(){
+    $.ajax({
+        url: "http://api.sl.se/api2/LineData.json?key=" + API_KEY + "&model=StopArea",
+        type: "GET",
+        dataType: "text",
+        success: function(data){
+            
+            var json_data = JSON.parse(data);
+            console.log(json_data.ResponseData.Result.length);
+            for(i = 0; i<json_data.ResponseData.Result.length; i++){
+            	var obj = json_data.ResponseData.Result[i];
+            	if(obj.StopAreaTypeCode === "BUSTERM"){
+            		listOfStations.push(obj.StopPointName);
+            	}
+            }
+
+            addToDatabase();
+
+        	
+        }
+    });
+
+}
+
+function addToDatabase(){
+	for(i = 0; i<listOfStations.length; i++){
+		database.addToStationDatabase(listOfStations[i]);
+	}
+}
+
+
+
+
